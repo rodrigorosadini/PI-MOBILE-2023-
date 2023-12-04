0d653a2df56aaf4cce60a1e18334f8014133bd5e
Rodrigo Lopes Rosadini Leite <rodrigorosadini2@gmail.com>
2023-12-04 02:57:08 -0300

    Carrossel de Imagens. Closes #8

--- laundry-app/components/Carousel.js
+++ laundry-app/components/Carousel.js
@@ -2,6 +2,8 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { SliderBox } from "react-native-image-slider-box";
 
+// CARROSEL DE IMAGENS
+
 const Carousel = () => {
   const images = [
     "https://media.istockphoto.com/id/1247884083/vector/laundry-service-room-vector-illustration-washing-and-drying-machines-with-cleansers-on-shelf.jpg?s=612x612&w=0&k=20&c=myaNEKlqX7R--bzWGDoMI7PhdxG_zdQTKYEBlymJQGk=",
